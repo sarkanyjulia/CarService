c8eb0889d4cd6caa3245cc04d9c7d6772bb250f3
sarkanyjulia <sjuli17@gmail.com>
2019-10-14 22:42:51 +0200

    Page titles, logout

---
+++ CarService/CarService.Website/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarService.Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CarService.Website.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly ICarServiceService _service;
+
+        public BaseController(ICarServiceService service)
+        {
+            _service = service;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+            ViewBag.CurrentPartnerName = String.IsNullOrEmpty(User.Identity.Name) ? null : User.Identity.Name;
+        }
+    }
+}
---
+++ CarService/CarService.Website/Models/RegistrationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarService.Website.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "A felhasználónév megadása kötelező.")]
+        [RegularExpression("^[A-Za-z0-9_-]{5,40}$", ErrorMessage = "A felhasználónév formátuma, vagy hossza nem megfelelő.")]
+        public String UserName { get; set; }
+        
+        [Required(ErrorMessage = "A jelszó megadása kötelező.")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        
+        [Required(ErrorMessage = "A jelszó ismételt megadása kötelező.")]
+        [Compare(nameof(Password), ErrorMessage = "A két jelszó nem egyezik.")]
+        [DataType(DataType.Password)]
+        public String ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "A név megadása kötelező.")] // feltételek a validáláshoz
+        [StringLength(60, ErrorMessage = "A név maximum 60 karakter lehet.")]
+        public String Name { get; set; }
+        
+        [Required(ErrorMessage = "A cím megadása kötelező.")]
+        public String Address { get; set; }
+
+        [Required(ErrorMessage = "A telefonszám megadása kötelező.")]
+        [Phone(ErrorMessage = "A telefonszám formátuma nem megfelelő.")]
+        [DataType(DataType.PhoneNumber)]
+        public String PhoneNumber { get; set; }
+    }
+}
