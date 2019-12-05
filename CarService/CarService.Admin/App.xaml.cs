e0afa5a2d5e817878e86b04994b204ae6dcec5d6
sarkanyjulia <sjuli17@gmail.com>
2019-12-05 22:52:30 +0100

    Login

--- CarService/CarService.Admin/App.xaml.cs
+++ CarService/CarService.Admin/App.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Win32;
+using CarService.Admin.Model;
+using CarService.Admin.Persistence;
+using CarService.Admin.View;
+using CarService.Admin.ViewModel;
+using System;
 
 namespace CarService.Admin
 {
@@ -13,5 +13,61 @@ namespace CarService.Admin
     /// </summary>
     public partial class App : Application
     {
+        private ICarServiceModel _model;
+        private LoginViewModel _loginViewModel;
+        private LoginWindow _loginView;
+        private MainViewModel _mainViewModel;
+        private MainWindow _mainView;
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+            Exit += new ExitEventHandler(App_Exit);
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            _model = new CarServiceModel(new CarServicePersistence("https://localhost:44300/")); // megadjuk a szolgáltatás címét
+
+            _loginViewModel = new LoginViewModel(_model);
+            _loginViewModel.ExitApplication += new EventHandler(ViewModel_ExitApplication);
+            _loginViewModel.LoginSuccess += new EventHandler(ViewModel_LoginSuccess);
+            _loginViewModel.LoginFailed += new EventHandler(ViewModel_LoginFailed);
+
+            _loginView = new LoginWindow();
+            _loginView.DataContext = _loginViewModel;
+            _loginView.Show();
+
+        }
+
+        private void ViewModel_LoginSuccess(object sender, EventArgs e)
+        {
+            _mainViewModel = new MainViewModel(_model);
+            
+
+            _mainView = new MainWindow();
+            _mainView.DataContext = _mainViewModel;
+            _mainView.Show();
+
+            _loginView.Close();
+        }
+
+        private void ViewModel_LoginFailed(object sender, EventArgs e)
+        {
+            MessageBox.Show("A bejelentkezés sikertelen.", "Szuper Szaki Autószervíz", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        public async void App_Exit(object sender, ExitEventArgs e)
+        {
+            if (_model.IsUserLoggedIn) // amennyiben be vagyunk jelentkezve, kijelentkezünk
+            {
+                await _model.LogoutAsync();
+            }
+        }
+
+        private void ViewModel_ExitApplication(object sender, System.EventArgs e)
+        {
+            Shutdown();
+        }
     }
 }
---
+++ CarService/CarService.Admin/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using CarService.Admin.Model;
+using CarService.Admin.Persistence;
+using CarService.Data;
+using System.Windows;
+
+namespace CarService.Admin.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ICarServiceModel _model;
+
+        public MainViewModel(ICarServiceModel model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            _model = model;
+        }
+    }
+}
